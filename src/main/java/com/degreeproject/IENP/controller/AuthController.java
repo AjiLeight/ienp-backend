646f9e99f292cf5ebef0ea0534702577335bba0f
Ajith <ajith.pgm89@gmail.com>
2023-01-06 18:53:09 +0530

    Login api created

--- src/main/java/com/degreeproject/IENP/controller/AuthController.java
+++ src/main/java/com/degreeproject/IENP/controller/AuthController.java
@@ -1,13 +1,20 @@
 package com.degreeproject.IENP.controller;
 
+import com.degreeproject.IENP.dto.AuthResponseDto;
+import com.degreeproject.IENP.dto.LoginDto;
 import com.degreeproject.IENP.dto.RegisterDto;
 import com.degreeproject.IENP.entity.Role;
 import com.degreeproject.IENP.entity.UserEntity;
 import com.degreeproject.IENP.repository.RoleRepository;
 import com.degreeproject.IENP.repository.UserRepository;
+import com.degreeproject.IENP.security.JwtGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +28,20 @@ public class AuthController {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     private RoleRepository roleRepository;
+    private AuthenticationManager authenticationManager;
+    private JwtGenerator jwtGenerator;
 
     @Autowired
     public AuthController(UserRepository userRepository,
                           PasswordEncoder passwordEncoder,
-                          RoleRepository roleRepository) {
+                          RoleRepository roleRepository,
+                          AuthenticationManager authenticationManager,
+                          JwtGenerator jwtGenerator) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.roleRepository = roleRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtGenerator = jwtGenerator;
     }
 
     @PostMapping("register")
@@ -48,4 +61,20 @@ public class AuthController {
         return new ResponseEntity<>("User Created Successfully", HttpStatus.OK);
 
     }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    loginDto.getUsername(),
+                    loginDto.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+
+        return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
+    }
+
 }
